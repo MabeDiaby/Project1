6f85088c8c7b03ea54324c3df91957d286139a65
Mabe-Diaby <diabymabe@gmail.com>
2021-11-08 15:09:29 -0500

    starter code html css

---
+++ script.js
@@ -0,0 +1,55 @@
+/*
+TODO:
+A short description of your game: 
+A single player game where the player will guess letters to create a hidden (random) word. if they guess correctly, they win, if they guess the wrong letter the spacemen lose.
+
+MVP Goals
+    [] Be able to either click letters or type on keyboards
+    [] Able to restart game with restart button
+    [] Able to show total amount of chances/guesses left
+    []  Nice clean game
+
+Stretch Goals
+    [] Animated astronaut and ship
+    [] Victory or losing animation
+    [] A timer
+    [] Random word generator with genre
+    [] Track scores across games (even if the page is reloaded)
+*/ 
+
+/*
+HTML Steps:
+    [x] Div class: Wrapper class: wraps the game in a box
+    [x] Make a Title
+    [x] Make p tags: Catagory's/Genre 
+    [x] Make a h3 tag: Choose a letter below
+    [x] Make divs: Lives Remaining, Restart Button, Info
+    [x] Make a Reset Button: Play Again
+    [x] Make an h3: Game Over, You Win
+BONUS
+    [] Timer
+    [] Random funfacts
+*/
+/*
+JS Steps:
+    [] 
+    [] 
+    [] 
+    [] 
+    [] 
+    [] 
+    [] 
+    [] 
+*/
+/*
+CSS Steps:
+    [] 
+    [] 
+    [] 
+    [] 
+    [] 
+    [] 
+    [] 
+    [] 
+*/
+
